c1338790d7f3c7729a7613ae47b7426633c3f5fa
j-pats <jessetpats@gmail.com>
2024-09-22 13:16:05 -0700

    Added Tanstack Query for all contacts
    Modified contact-app package file to use a proxy to avoid cross-reference error.
    Modified express-server "/contacts" endpoint to return json file of all contacts
    Modified contact-app index component to use tanstack query to get all contacts from express-server, and format contacts into a list
    modified contacts-app main.tsx to use query client

--- contacts-app/src/routes/index.tsx
+++ contacts-app/src/routes/index.tsx
@@ -1,14 +1,53 @@
+import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { useState, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
+export type ContactType = {
+  id: string
+  name: string
+  email: string
+  phone: string
+}
+
 function Index() {
-  return (
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const response = await fetch(
+        'http://localhost:3000/contacts',
+      )
+      return await response.json();
+    },
+  })
+
+  if (isPending) return (
     <div>
-      <h1>Contacts</h1>
-      <p>This is where contacts go in a table</p>
+      Loading...
     </div>
   )
+
+  if (error) {
+    console.log(data);
+    return (
+      <div>
+        'An error has occurred: ' + {error.message}
+      </div>
+    )
+  }
+  
+  return (
+    <div>
+        {data.contacts.map((contact:ContactType)=>{
+          return (
+            <div key={contact.id}>
+              <p><strong>{contact.id}</strong>: {contact.name} Email: {contact.email} </p>
+            </div>
+          )
+        })}
+    </div>
+  );
 }
--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -3,9 +3,13 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Typescript express server');
+var jsonFile = require("../db.json");
+
+app.get('/contacts', (req: Request, res: Response) => {
+    res.json(jsonFile);
   });
+
+  
   
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
