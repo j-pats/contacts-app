96a9d8c3f9b40bfc6e3df0b8893807de0fe22753
j-pats <jessetpats@gmail.com>
2024-09-25 16:36:05 -0700

    Added Docker files
    Dockerfiles added for web app and server
    docker ignore files added to ignore node_modules
    Modified web app package.json file to use host 0.0.0.0
    Fixed typo in web app files (renamed css file to index.css, updated import statements)

--- contacts-app/src/routes/__root.tsx
+++ contacts-app/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
-import "../Index.css"
+import "../index.css"
 
 export const Route = createRootRoute({
   component: () => (
